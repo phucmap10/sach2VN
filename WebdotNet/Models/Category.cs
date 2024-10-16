c69dc871ce3ef6069c7d44dd0bf5f9b04edb98fb
Pepes0cool <kaizx8@gmail.com>
2024-10-16 12:15:46 +0700

    make model

---
+++ WebdotNet/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace WebdotNet.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; } //if we set this as the 'classname'+'entity' this will be treated as primary key
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
