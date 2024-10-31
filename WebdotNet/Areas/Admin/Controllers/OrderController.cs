043be4b2d491475ad2b10ef531cda27be4ee08e4
Pepes0cool <kaizx8@gmail.com>
2024-10-31 17:21:11 +0700

    manage order

--- WebdotNet/Areas/Admin/Controllers/OrderController.cs
+++ WebdotNet/Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using WebdotNet.DataAccess.Repository;
 using WebdotNet.DataAccess.Repository.IRepository;
 using WebdotNet.Models;
+using WebdotNet.Utility;
 
 namespace WebdotNet.Areas.Admin.Controllers
 {
@@ -23,11 +25,50 @@ namespace WebdotNet.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrder = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser").ToList();
-            return Json(new { data = objOrder });
+            IEnumerable<OrderHeader> objOrderHeaders;
+
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unitOfWork.OrderHeader
+                    .GetAll(u => u.ApplicationUserID == userId, includeProperties: "ApplicationUser");
+            }
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApprove);
+                    break;
+                default:
+                    break;
+
+            }
+
+
+            return Json(new { data = objOrderHeaders });
         }
         #endregion
+
+
     }
 }
--- WebdotNet/wwwroot/js/order.js
+++ WebdotNet/wwwroot/js/order.js
@@ -1,31 +1,51 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
-});
-function loadDataTable() {
-        dataTable = $('#tblData').DataTable({
-            "ajax": {url :'/admin/order/getall'
-        },
-            "columns": [
-                { data: 'id', "width" : "15%" },
-                { data: 'name', "width": "10%" },
-                { data: 'address', "width": "15%" },
-                { data: 'phoneNumber', "width": "10%" },
-                { data: 'orderStatus', "width": "15%" },
-                {data: 'orderTotal', "width":"15%"},
-                {
-                    data: 'orderTotal',
-                    "render": function (data) {
-                        return `<div class="w-75 btn-group" role="group">
-                        <a href="/Admin/Order/Edit/?id=${data}" class="btn btn-primary mx-2">
-                            <i class="bi bi-pencil-square"></i>Edit
-                        </a>
-                    </div>`;
-                    },
-                    "width": "20%"
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
                 }
-        ]
-        });
-}
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
 
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall?status=' + status },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'address', "width": "20%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
+                    
+                    </div>`
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
