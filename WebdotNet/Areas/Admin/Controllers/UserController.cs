e529825bbeaca3d97b50f9f131fc6901ee253a8b
Pepes0cool <kaizx8@gmail.com>
2024-11-04 14:28:06 +0700

    make user management

---
+++ WebdotNet/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using WebdotNet.DataAccess.Repository.IRepository;
+using WebdotNet.DataAccess.Data;
+using WebdotNet.Models;
+using WebdotNet.Models.ViewModels;
+using WebdotNet.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork, RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll().ToList();
+
+            return Json(new { data = objUserList });
+        }
+
+        #endregion
+}
+}
---
+++ WebdotNet/wwwroot/js/user.js
@@ -0,0 +1,18 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "address", "width": "15%" },
+            { "data": "userName", "width": "15%" },
+        ]
+    });
+}
