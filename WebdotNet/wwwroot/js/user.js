2e4d2ffb3ea24f6d39f98a2c4008d3cfc30f47dd
Pepes0cool <kaizx8@gmail.com>
2024-11-04 14:32:36 +0700

    fix user table

--- WebdotNet/wwwroot/js/user.js
+++ WebdotNet/wwwroot/js/user.js
@@ -9,10 +9,10 @@ function loadDataTable() {
         "ajax": { url: '/admin/user/getall' },
         "columns": [
             { "data": "name", "width": "15%" },
-            { "data": "email", "width": "15%" },
+            { "data": "email", "width": "30%" },
             { "data": "phoneNumber", "width": "15%" },
-            { "data": "address", "width": "15%" },
-            { "data": "userName", "width": "15%" },
+            { "data": "address", "width": "30%" },
+            { "data": "userName", "width": "10%" }
         ]
     });
 }
